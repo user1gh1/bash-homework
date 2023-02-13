36385312b5d87aba237c25ab9f15291d39d9a935
Albert <user1gm1@gmail.com>
2023-02-14 03:55:40 +0400

    add functions and main function

--- homework1.sh
+++ homework1.sh
@@ -1,27 +1,58 @@
 #!/bin/bash
-      
+function current_user {     
 echo -e "Currently logged users: $(whoami)\n"
-
+}
+function shell_dir {
 echo -e "Shell directory: $SHELL\n"
-
+}
+function home_dir {
 echo -e "Home directory: $HOME\n"
-
+}
+function os {
 echo -e "OS $(lsb_release -d)\n"
-
+}
+function current_pwd {
 echo -e "Current working directory: $(pwd)\n"
-
+}
+function num_logged_users {
 echo -e "Number of users logged in: $(who | wc -l)\n"
-
+}
+function avail_shells {
 echo -e "Available shells in system:\n$(cat /etc/shells)\n"
-
+}
+function hard_disk {
 echo -e "Hard disk information:\n $(df -h) \n"
-
-echo -e "CPU information:\n$(lscpu | grep -v 'Flags' | grep -v 'Vulnerability')\n"
-
+}
+function cpu_info {
+echo -e "CPU info:\n$(lscpu | grep -v 'Flags' | grep -v 'Vulnerability')\n"
+}
+function memory_info {
 echo -e "Memory information:\n$(free -h)\n"
-
+}
+function file_sys_info {
 echo -e "File system information:\n$(lsblk | grep "sd")"
-
+}
+function current_run_procs {
 echo -e "\nCurrently running processes:"
 ps -a
+}
+
+function main {
+	current_user
+	shell_dir
+	home_dir
+	os
+	current_pwd
+	num_logged_users
+	avail_shells
+	hard_disk
+	cpu_info
+	memory_info
+	file_sys_info
+	current_run_procs
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    main "$@"
+fi
 
---
+++ homework2mv.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+function read_file {
+echo -n "Enter the full path to file "
+read file
+}
+
+function paste_file {
+echo -n "Enter the full path to copy "
+read copy
+}
+
+#create a hard link and after delete original file 
+function hard_link_copy {
+ln $file $copy
+if [ $? -eq 0 ]; then
+	delete_file
+fi
+}
+
+function delete_file {
+rm $file
+}
+
+function main {
+read_file
+paste_file
+hard_link_copy
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    main "$@"
+fi
+
