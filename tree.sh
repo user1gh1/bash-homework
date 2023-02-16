a187f9848128d0de3d90848ec8817087852f2d4d
Albert <user1gm1@gmail.com>
2023-02-17 01:54:48 +0400

    Update -> tree don't show * (empty). Correct colorize executable files, symbolic links, dirs

--- tree.sh
+++ tree.sh
@@ -12,12 +12,18 @@ function tree() {
 
     for file in "$location"/*; do
         if [ -d "$file" ]; then
-            echo -e "$tabb\033[34m $(basename "$file")\033[0m" # color text blue 
-
+            echo -e "$tabb\033[34m $(basename "$file")\033[0m" # color text blue if dir 
 			tree "$file" "$tabb$tabbb"
-		#	tree "$file" $((tabb=${tabb}${tabbb})) 
+
+		elif [ "$(basename "$file")" == '*' ]; then #empty dir continue 
+            continue        
+		elif [ -x "$file" ]; then
+            echo -e "$tabb\033[32m $(basename "$file")\033[0m" # color text green if executable file
+		elif [ -h "$file" ]; then
+            echo -e "$tabb\033[36m$(basename "$file")\033[0m" # color light blue if symbolic link
         else
-            echo -e "$tabb\033[32m $(basename "$file")\033[0m" # color text green 
+            echo -e "$tabb $(basename "$file")"  
+			
         fi
     done
 
